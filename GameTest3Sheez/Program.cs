a1c0ebbd98e77d1317b350d4a0a3b482b753a3a3
Wafflebasket <luke.keijer@mail.com>
2018-03-26 00:52:20 +0200

    Add project files.

---
+++ GameTest3Sheez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameTest3Sheez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
